10a15279a0515b3fe2eadcb551c1714ce821ad6a
Cbhihe <cedric.bhihe@gmail.com>
2017-05-24 21:51:23 +0200

    Complete lab4 - cedric

---
+++ Lab4/ourService_pohl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+import SocketServer
+
+class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+   def do_GET(self):
+      if self.path == '/':
+         self.path = '/ourService.html'
+      return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = MyRequestHandler
+server = SocketServer.TCPServer(('0.0.0.0', 8954), Handler)
+
+print 'Started my REST API on port 8954' 
+server.serve_forever()
